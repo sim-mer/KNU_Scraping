572b913b474838286f73003dd620e543556ecbd5
sim-mer <gyu2781@gmail.com>
2024-06-28 15:48:45 +0900

    fix: Timezone 변경

--- src/main/java/org/simmer/knu_scraping/KnuScrapingApplication.java
+++ src/main/java/org/simmer/knu_scraping/KnuScrapingApplication.java
@@ -1,10 +1,18 @@
 package org.simmer.knu_scraping;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class KnuScrapingApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KnuScrapingApplication.class, args);
