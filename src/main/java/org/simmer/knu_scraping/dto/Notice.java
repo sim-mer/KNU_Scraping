7876043bb179dcf04eb42c3e05974e9b61648082
sim-mer <gyu2781@gmail.com>
2024-07-01 19:43:43 +0900

    feat: Jsoup Element -> Notice 정적 팩토리 메서드 작성

--- src/main/java/org/simmer/knu_scraping/dto/Notice.java
+++ src/main/java/org/simmer/knu_scraping/dto/Notice.java
@@ -1,4 +1,26 @@
 package org.simmer.knu_scraping.dto;
 
+import org.jsoup.nodes.Element;
+import org.simmer.knu_scraping.domain.MajorEnum;
+
 public record Notice (String tag, String title, String date, String link) {
+    public static Notice of(Element e, MajorEnum major) {
+        String tag = "없음";
+        String url = e.select(major.link).attr("href");
+
+        if(major.tag != null) {
+            tag = e.select(major.tag).text();
+        }
+
+        if(major.siteFactory != null) {
+            url = major.siteFactory.createLink(url);
+        }
+
+        return new Notice(
+                tag,
+                e.select(major.title).text(),
+                e.select(major.date).text(),
+                url
+        );
+    }
 }
