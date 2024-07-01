cfad0b7b98cdc344beaf6063650da1b221ac387a
sim-mer <gyu2781@gmail.com>
2024-07-01 19:42:35 +0900

    feat: MajorEnum - 학사 공지 관련 정보 추가 - 게시글 번호 가져오는 로직 변경에 따른 currentNoticeNum selector 변경 - siteFactory 필드 추가

--- src/main/java/org/simmer/knu_scraping/domain/MajorEnum.java
+++ src/main/java/org/simmer/knu_scraping/domain/MajorEnum.java
@@ -1,15 +1,27 @@
-package org.simmer.knu_scraping.config;
+package org.simmer.knu_scraping.domain;
 
 public enum MajorEnum {
     COMPUTER_SCIENCE_AND_ENGINEERING(
             "https://cse.knu.ac.kr/bbs/board.php?bo_table=sub5_1",
             "",
-            "#fboardlist > div > table > tbody > tr:nth-child(1) > td.td_num2",
+            "td.td_num2",
             "#fboardlist > div > table > tbody > tr",
             "td.td_subject > a",
             "td.td_subject > div > a",
             "td.td_datetime.hidden-xs",
-            "td.td_subject > div > a"
+            "td.td_subject > div > a",
+            null
+    ),
+    KNU_ACADEMIC(
+            "https://www.knu.ac.kr/wbbs//wbbs/bbs/btin/stdList.action?btin.page=57&menu_idx=42",
+            "https://discord.com/api/webhooks/1255930388241448990/8ylzq2hw6ARc97OzTVafjzrTWozeax4qby_tZpICpkbQdaPnYzNBCsxKJ_f0MVRJMUgg",
+            "td.num",
+            "#btinForm > div > table > tbody > tr",
+            null,
+            "td.subject > a",
+            "td.date",
+            "td.subject > a",
+            new KnuAcademic()
     );
 
     public final String url;
@@ -20,8 +32,9 @@ public enum MajorEnum {
     public final String title;
     public final String date;
     public final String link;
+    public final SiteFactory siteFactory;
 
-    MajorEnum(String url, String webhookUrl, String currentNoticeNum, String noticeList, String tag, String title, String date, String link) {
+    MajorEnum(String url, String webhookUrl, String currentNoticeNum, String noticeList, String tag, String title, String date, String link, SiteFactory siteFactory) {
         this.url = url;
         this.webhookUrl = webhookUrl;
         this.currentNoticeNum = currentNoticeNum;
@@ -30,5 +43,6 @@ public enum MajorEnum {
         this.title = title;
         this.date = date;
         this.link = link;
+        this.siteFactory = siteFactory;
     }
 }
