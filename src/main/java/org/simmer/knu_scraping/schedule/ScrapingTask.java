70dd2f8677f1a883c803c4c752fa701eb3498ea6
sim-mer <gyu2781@gmail.com>
2024-07-24 15:09:37 +0900

    refactor: 리팩토링 내용 Scraping class에 반영

--- src/main/java/org/simmer/knu_scraping/schedule/ScrapingTask.java
+++ src/main/java/org/simmer/knu_scraping/schedule/ScrapingTask.java
@@ -1,10 +1,12 @@
 package org.simmer.knu_scraping.schedule;
 
-import lombok.RequiredArgsConstructor;
-import org.simmer.knu_scraping.domain.MajorEnum;
-import org.simmer.knu_scraping.domain.HtmlDocument;
-import org.simmer.knu_scraping.dto.Notice;
-import org.simmer.knu_scraping.util.DiscordClient;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.simmer.knu_scraping.schedule.dto.HtmlDocument;
+import org.simmer.knu_scraping.schedule.dto.Notice;
+import org.simmer.knu_scraping.client.DiscordClient;
+import org.simmer.knu_scraping.util.webhook.WebhookGenerator;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,24 +15,33 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class ScrapingTask {
+
     private final DiscordClient discordClient;
+    private final Map<Major, WebhookGenerator> webhookGenerators;
     private final RedisTemplate<String, String> redisTemplate;
+
+    public ScrapingTask(DiscordClient discordClient, List<WebhookGenerator> webhookGenerators,
+        RedisTemplate<String, String> redisTemplate) {
+        this.discordClient = discordClient;
+        this.webhookGenerators = webhookGenerators.stream()
+            .collect(Collectors.toUnmodifiableMap(WebhookGenerator::major, Function.identity()));
+        this.redisTemplate = redisTemplate;
+    }
 
 
     @Scheduled(cron = "0 0/5 7-19 * * MON-FRI")
     public void scraping() {
-        for (MajorEnum major : MajorEnum.values()) {
-            HtmlDocument htmlDocument = new HtmlDocument(major);
+        for (Major major : Major.values()) {
+            HtmlDocument htmlDocument = new HtmlDocument(major.htmlSelector);
 
             int currentNoticeNum = htmlDocument.getCurrentNoticeNum();
 
             int recentNum = getRecentNum(currentNoticeNum, major.ordinal());
 
-            if(currentNoticeNum > recentNum) {
+            if (currentNoticeNum > recentNum) {
                 List<Notice> notices = htmlDocument.getNotices(currentNoticeNum - recentNum);
-                discordClient.sendDiscordMessage(notices, major);
+                discordClient.sendDiscordMessage(notices, webhookGenerators.get(major));
             }
         }
     }
@@ -41,7 +52,7 @@ public class ScrapingTask {
 
         values.set(String.valueOf(major), String.valueOf(currentNoticeNum));
 
-        if(recent == null) {
+        if (recent == null) {
             return currentNoticeNum;
         }
 
